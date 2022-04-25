bebc948a50b3db23e08cffb4c554415b07a0e728
Сергей Щикунов <admod32@gmail.com>
2022-04-25 19:40:35 +0300

    Create main file

---
+++ guess.py
@@ -0,0 +1,31 @@
+#  Это игра по угадыванию чисел.
+import random
+
+guessesTaken = 0
+
+print("Привет! Как тебя зовут?")
+myName = input()
+
+number = random.randint(1, 20)
+print('Что ж, ' + myName + ', я загадываю число от 1 до 20.')
+
+for guessesTaken in range(6):
+  print('Попробуй угадать.')
+  guess = int(input())
+
+  if guess < number:
+    print('Твоё число слишком маленькое...')
+
+  if guess > number:
+    print('Твоё число слишком большое...')  
+  
+  if guess == number:
+    break
+
+if guess == number:
+  guessesTaken = str(guessesTaken + 1)
+  print('Отлично, ' + myName + '! Ты справился за ' + guessesTaken + ' попытки!')
+
+if guess != number:
+  number = str(number)
+  print('Увы. Я загадал число ' + number + '.')
